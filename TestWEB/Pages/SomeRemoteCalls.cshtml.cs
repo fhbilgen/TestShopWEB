570f193b5176a1418306b68884175f988f2c487a
Faik Hakan Bilgen <faikb@microsoft.com>
2025-07-24 09:50:44 +0300

    Getting API urls from environment variables

--- TestWEB/Pages/SomeRemoteCalls.cshtml.cs
+++ TestWEB/Pages/SomeRemoteCalls.cshtml.cs
@@ -7,10 +7,15 @@ namespace TestWEB.Pages
     {
         private static readonly HttpClient client = new HttpClient();
 
-        private static string reqUrl = "https://testshopapi.azurewebsites.net/api/LongExecution/longexecution";
-        private static string reqUrl2 = "https://testshopapi.azurewebsites.net/api/exception/throwexception";
-        private static string reqUrl3 = "https://testshopapi.azurewebsites.net/api/LongExecution/DoubleHop";
-        private static string reqUrl4 = "https://testshopapi.azurewebsites.net/api/LongExecution/ChattyDoubleHop";
+        //private static string reqUrl = "https://testshopapi.azurewebsites.net/api/LongExecution/longexecution";
+        //private static string reqUrl2 = "https://testshopapi.azurewebsites.net/api/exception/throwexception";
+        //private static string reqUrl3 = "https://testshopapi.azurewebsites.net/api/LongExecution/DoubleHop";
+        //private static string reqUrl4 = "https://testshopapi.azurewebsites.net/api/LongExecution/ChattyDoubleHop";
+
+        private static string reqUrl = Environment.GetEnvironmentVariable("TestShopApi_LongExecution"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/longexecution";
+        private static string reqUrl2 = Environment.GetEnvironmentVariable("TestShopApi_ThrowException"); // ?? "https://testshopapi.azurewebsites.net/api/exception/throwexception";
+        private static string reqUrl3 = Environment.GetEnvironmentVariable("TestShopApi_DoubleHop"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/DoubleHop";
+        private static string reqUrl4 = Environment.GetEnvironmentVariable("TestShopApi_ChattyDoubleHop"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/ChattyDoubleHop";
 
         public void OnGet()
         {
