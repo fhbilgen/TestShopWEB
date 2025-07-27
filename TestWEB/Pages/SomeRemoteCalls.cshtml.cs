32c61d22111a3093225b272bfdc59cca74ff3a4b
Faik Hakan Bilgen <faikb@microsoft.com>
2025-07-27 17:59:52 +0300

    .NET 9

--- TestWEB/Pages/SomeRemoteCalls.cshtml.cs
+++ TestWEB/Pages/SomeRemoteCalls.cshtml.cs
@@ -12,10 +12,10 @@ namespace TestWEB.Pages
         //private static string reqUrl3 = "https://testshopapi.azurewebsites.net/api/LongExecution/DoubleHop";
         //private static string reqUrl4 = "https://testshopapi.azurewebsites.net/api/LongExecution/ChattyDoubleHop";
 
-        private static string reqUrl = Environment.GetEnvironmentVariable("TestShopApi_LongExecution"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/longexecution";
-        private static string reqUrl2 = Environment.GetEnvironmentVariable("TestShopApi_ThrowException"); // ?? "https://testshopapi.azurewebsites.net/api/exception/throwexception";
-        private static string reqUrl3 = Environment.GetEnvironmentVariable("TestShopApi_DoubleHop"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/DoubleHop";
-        private static string reqUrl4 = Environment.GetEnvironmentVariable("TestShopApi_ChattyDoubleHop"); // ?? "https://testshopapi.azurewebsites.net/api/LongExecution/ChattyDoubleHop";
+        private static string reqUrl = Environment.GetEnvironmentVariable("TestShopApi_LongExecution"); 
+        private static string reqUrl2 = Environment.GetEnvironmentVariable("TestShopApi_ThrowException"); 
+        private static string reqUrl3 = Environment.GetEnvironmentVariable("TestShopApi_DoubleHop"); 
+        private static string reqUrl4 = Environment.GetEnvironmentVariable("TestShopApi_ChattyDoubleHop"); 
 
         public void OnGet()
         {
